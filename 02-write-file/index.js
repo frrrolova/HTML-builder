b077a0747ecf3e7bf74a9de3068ece833bde8a24
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-05-05 21:28:51 +0200

    feat: 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const readLine = require('readline');
+
+const writeStream = fs.createWriteStream(__dirname + '/destination.txt');
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Type something: '
+});
+
+rl.prompt();
+rl.on(
+  'line',
+  (input) => {
+    if (input === 'exit') {
+      console.log('Good bye!');
+      rl.close();
+      return;
+    }
+    writeStream.write(input);
+    rl.prompt();
+  }
+);
+
+rl.on(
+  'SIGINT',
+  () => {
+    console.log('\nGood bye!');
+    rl.close();
+  }
+);
+
+
+
