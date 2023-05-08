96f03c1d5f2a906f51fcf9d1e47ab9229805774a
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-05-08 12:30:28 +0200

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,13 @@
+const { readdir, mkdir, copyFile } = require('node:fs/promises');
+const folderCopy = __dirname + '/files-copy';
+
+mkdir(folderCopy, { recursive: true })
+  .then(() => {
+    readdir(__dirname + '/files').then((files) => {
+      files.forEach((file) => {
+        copyFile(__dirname + '/files/' + file, folderCopy + '/' + file);
+      });
+    });
+  });
+
 
