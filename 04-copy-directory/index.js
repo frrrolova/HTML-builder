335ee353d58f5d29ab12a3a5c7783cd2fad37116
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-05-09 00:00:55 +0200

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,13 +1,14 @@
-const { readdir, mkdir, copyFile } = require('node:fs/promises');
+const { readdir, mkdir, copyFile, rm } = require('node:fs/promises');
 const folderCopy = __dirname + '/files-copy';
 
-mkdir(folderCopy, { recursive: true })
-  .then(() => {
-    readdir(__dirname + '/files').then((files) => {
-      files.forEach((file) => {
-        copyFile(__dirname + '/files/' + file, folderCopy + '/' + file);
-      });
+rm(folderCopy, { recursive: true })
+  .then(() => mkdir(folderCopy, { recursive: true }))
+  .then(() => readdir(__dirname + '/files'))
+  .then((files) => {
+    files.forEach((file) => {
+      copyFile(__dirname + '/files/' + file, folderCopy + '/' + file);
     });
   });
+
 
 
